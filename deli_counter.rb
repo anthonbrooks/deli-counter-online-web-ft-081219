ddc6ea7e9637b4689ee97499c4195750ba65ac24
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-15 16:23:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def line (deli_line)
     puts "The line is currently empty."
 
   else
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     deli_line.each.with_index(1) do |person, pos|
-      current_line << "#{pos}. #{person}"
+      current_line << " #{ pos }. #{ person }"
     end
   puts current_line
   end
@@ -16,7 +16,7 @@ end
 
 def take_a_number (katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{ katz_deli.length } in line."
+  puts "Welcome, #{ name }. You are number #{ katz_deli.length } in line."
 end
 
 def now_serving (katz_deli)
