14befc014ef38d6cbf707afec0c881fd8fd9457a
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-15 16:01:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line (deli_line)
   
   if katz_deli.length == 0
     return "The line is currently empty."
-  end
-  if katz_deli.length > 0 
+
+  elsif katz_deli.length > 0 
     return "The line is currently: #{katz_deli[0] + 1}. #{katz_deli}"
   end
   
