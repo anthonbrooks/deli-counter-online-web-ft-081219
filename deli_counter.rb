64850a71ae04f9525887fa6a794259058b38e303
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-15 16:09:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def line (deli_line)
     puts "The line is currently empty."
 
   else
-    puts "The line is currently: #{katz_deli[0] + 1}. #{katz_deli}"
+    current_line "The line is currently: #{katz_deli[0] + 1}. #{katz_deli}"
   end
-  
+  current_line
 end
 
 def take_a_number (katz_deli, name)
