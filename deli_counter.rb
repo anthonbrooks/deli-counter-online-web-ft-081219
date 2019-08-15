2b7cdd15b818f70cff7176d8fb4e8ff3e9eec629
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-15 15:28:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line (deli_line)
   
   if katz_deli.length == 0
     puts "The line is currently empty."
-  if katz_deli.length 
+  if katz_deli.length > 0 
     return "The line is currently: #{katz_deli[0] + 1}. #{katz_deli}"
   end
 end
